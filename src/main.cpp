cb4bce1c83038daa5856623d039bc335c416ab70
Scrappyz <ghostmic3000@gmail.com>
2024-09-06 15:47:46 +0800

    Added options

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,18 @@ namespace path = os::path;
 
 int main(int argc, char** argv)
 {
-    std::string command = R"(D:\"My Files"\Codes\C++\Tools\ctemplate\bin\Release\ctemplate.exe info "python")";
-    std::string output;
-    std::cout << (os::execute(command, output)) << std::endl;
-    std::cout << output << std::endl;
+    CLI::App app("Updater");
+    app.set_version_flag("-v,--version", "v0.1.0");
+
+    std::vector<std::string> caller_info;
+    std::string url;
+    app.add_option("-c,--caller", caller_info, "Path to the source caller and its current version.\n(E.g. \"path/to/caller.exe\" \"v1.0.0\")")->expected(2);
+    app.add_option("-u,--url", url, "URL to get the update from.")->expected(1);
+
+    CLI11_PARSE(app, argc, argv);
+
+    // Get update from url then place it in the caller path with a unique name.
+    // Delete the caller executable then rename the new executable.
 
     return 0;
 }
